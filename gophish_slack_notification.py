a6189d6377031bada0d3b599d1b32357235f0352
Arshaad Mohiadeen <arshaad919@gmail.com>
2022-06-21 14:14:42 +0800

    Create gophish_slack_notification.py

---
+++ gophish_slack_notification.py
@@ -0,0 +1,27 @@
+from email import message
+import requests
+import csv
+from distutils.command.config import config
+from gophish import Gophish
+from gophish.models import *
+import pandas as pd
+import random
+import datetime
+
+
+
+# GoPhish Config
+api_key = '<GOPHISH API KEY>'
+api = Gophish(api_key,host='<GOPHISH URL>',verify=True)
+
+summary = api.campaigns.summary(campaign_id=397)
+summary_breakdown = summary.stats.as_dict()
+
+payload = '{"text":"*Total emails sent*: %s \n _Clicked_: %s \n *_Submitted data_*: %s"}' % (summary_breakdown['total'],summary_breakdown['clicked'],summary_breakdown['submitted_data'])
+
+
+response = requests.post(
+    '<SLACK WEBHOOK>',
+    data = payload
+)
+print(response.text)
